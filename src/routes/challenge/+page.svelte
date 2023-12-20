ca31631838414ab7973e774f52630d0538c4c69f
Wiktoria van Harneveldt <62669899+wiktoriavh@users.noreply.github.com>
2023-12-20 15:05:01 +0100

    feat: read and render markdown files (#13)
    * feat: read and render markdown files
    
    * chore(deps): remove marked

---
+++ src/routes/challenge/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data;
+  console.log(data);
+</script>
+
+<article>
+  <h1>hello</h1>
+{#each data.challenges as challenge}
+  <li><a href={`/challenge/${challenge}`}> {challenge} </a></li>
+{/each}
+</article>

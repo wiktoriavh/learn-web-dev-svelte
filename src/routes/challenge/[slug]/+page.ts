a8ce954494906889857138978bf45a9c1722815b
Wiktoria Van Harneveldt <w.dev+wslgit@mailbox.org>
2024-02-24 16:11:30 +0100

    chore: remove console logs

--- src/routes/challenge/[slug]/+page.ts
+++ src/routes/challenge/[slug]/+page.ts
@@ -1,14 +1,11 @@
 import type { PageLoad } from './$types';
 
 const getSlugFromPath = (path: string) => {
-	console.log('path', path);
 	return path.match(/([^/]*)\.(md|svx)/)?.[1] ?? null;
 };
 
 export const load = (async ({ params }) => {
 	const modules = import.meta.glob(`/src/content/challenge/*.{md,svx}`);
-
-console.log('modules', Object.entries(modules).find(([path]) => getSlugFromPath(path) === params.slug));
 
 	const match = Object.entries(modules).find(([path]) => getSlugFromPath(path) === params.slug); // boolean true or false
 
@@ -17,6 +14,7 @@ console.log('modules', Object.entries(modules).find(([path]) => getSlugFromPath(
 	}
 
 	const [_path, module] = match;
+	// todo: fix this type, this looks wrongly typed
 	const post = await module();
 
 	return {
